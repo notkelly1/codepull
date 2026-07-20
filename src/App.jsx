af41939fc1049ad5afeb2ddb867ddf8ced5bb653
Kelly Wang <kelly.wangwzs@gmail.com>
2026-07-20 00:09:49 -0400

    basic gacha capabilities
    placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+{/*used to remember things between renders instead of forgetting them instantly*/}
+import {useState} from 'react' 
+import {CREATURES} from './creatures'
+
 function App() {
+  const [selectedCreature, setSelectedCreature] = useState(null)
+
+  {/* function that handles the gacha roll, picks a random index into the CREATURES array and calls setSelectedCreature, triggering react to update the UI with new value*/}
+  function handleRoll() {
+    const randomIndex = Math.floor(Math.random() * CREATURES.length)
+    setSelectedCreature(CREATURES[randomIndex])
+  }
+
   return (
     <div className="app">
       <h1>Codepull</h1>
+      {/* button that calls handleRoll function when clicked, triggering a new gacha roll */}
+      <button onClick={handleRoll}>Roll</button>
+      {/* if selectedCreature is not null, display the creature's emoji, name, and rarity */}
+      {selectedCreature && (
+        <div>
+          <div className='emoji'>{selectedCreature.emoji}</div>
+          <h2>{selectedCreature.name}</h2>
+          <p>Rarity: {selectedCreature.rarity}</p>
+        </div>
+      )}
     </div>
   )
 }
--- src/creatures.js
+++ src/creatures.js
@@ -1,3 +1,8 @@
 export const CREATURES = [
-    { id: 1, name: 'Dragon', rarity: 'common', habitat: 'Mountains' },
+  { id: 1, name: "Emberling", rarity: "common", emoji: "🔥" },
+  { id: 2, name: "Aqualet", rarity: "common", emoji: "💧" },
+  { id: 3, name: "Sproutle", rarity: "common", emoji: "🌱" },
+  { id: 4, name: "Voltbit", rarity: "rare", emoji: "⚡" },
+  { id: 5, name: "Frostbyte", rarity: "rare", emoji: "❄️" },
+  { id: 6, name: "Shadewisp", rarity: "legendary", emoji: "🌑" },
 ]
