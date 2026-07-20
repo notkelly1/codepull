164e8bb69eea3017ab7a526311c50b085ccf5882
Kelly Wang <kelly.wangwzs@gmail.com>
2026-07-20 14:25:22 -0400

    calculate gacha pull currency
    updated with conversion of coding time to pull

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import './App.css'
 function App() {
   //declaring it in App.jsx moves the collection state up to the App component so it can be shared between RollPage and CollectionPage, instead of being local to RollPage
   const [collection, setCollection] = useState([])
+
   //moves the codingStats state up to the App component (similarly to the others)
   const [codingStats, setCodingStats] = useState(null)
   
@@ -19,7 +20,26 @@ function App() {
       .then(data => setCodingStats(data))
       .catch(error => console.error(error))
   }, [])
+
+  //compute currency as dervied state
+  //following derived values calculate total minutes coded and total pulls earned based on the codingStats data fetched from Hackatime API (freshly fetched data is stored in codingStats state variable, which is updated when the fetchCodingStats function resolves successfully)
+  const MINUTES_PER_PULL = 10
+
+  const totalMinutesCoded = codingStats
+    ? Math.floor(codingStats.data.total_seconds / 60)
+    : 0
+
+  const totalPullsEarned = Math.floor(totalMinutesCoded / MINUTES_PER_PULL)
   
+  //track how many pulls have already been spent
+  const [pullsSpent, setPullsSpent] = useState(0)
+
+  const pullsAvailable = totalPullsEarned - pullsSpent
+
+  function spendPull() {
+    setPullsSpent((prev) => prev + 1)
+  }
+
   //creating new array with the new creature added to the end of the previous collection
   function addToCollection(creature) {
     setCollection(prevCollection => [...prevCollection, creature])
@@ -38,7 +58,15 @@ function App() {
 
       <Routes>
         {/*passing the property (prop) to the RollPage*/}
-        <Route path="/" element={<RollPage addToCollection={addToCollection} />} />
+        <Route path="/" element={<RollPage 
+              //pass balance information down to RollPage
+              addToCollection={addToCollection} 
+              pullsAvailable={pullsAvailable} 
+              spendPull={spendPull} 
+            />
+          } 
+        /> {/*closes route for RollPage*/}
+        
         {/*passing the property (prop) to the CollectionPage*/}
         <Route path="/collection" element={<CollectionPage collection={collection} />} />
         {/*passing the property to creatureDetailPage,id is a dynamic parameter, can be any value, used to identify which creature to show details for*/}
